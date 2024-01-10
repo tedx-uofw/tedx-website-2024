b3c4d5977f712cf3bf792935c3204caf74a9af4c
Harmony Shirk <harmonyshirk@gmail.com>
2024-01-09 20:07:48 -0800

    minor fixes

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,5 +1,26 @@
 import React from 'react';
 
+// edit this omg
+const times = ["10:00-11:00AM", "12:00-1:00PM", "1:00-2:00PM"]
+const names = ["SUSIE SHARPE", "SUSIE SHARPE", "SUSIE SHARPE"]
+const titles = ["Artist & Internal Medicine Physician", "Artist & Internal Medicine Physician", "Artist & Internal Medicine Physician"]
+
+function ScheduleItems(props) {
+    const time = props.times;
+    const name = props.names;
+    const title = props.titles;
+    return (
+        <li>
+            <div className='schedule-times'>
+
+            </div>
+            <div className='name-container'>
+
+            </div>
+        </li>
+    )
+}
+
 function Landing(){
     return (
         <div className = "landing-container">
@@ -11,6 +32,31 @@ function Landing(){
                     from a wide range of perspectives, including personal growth and innovation.Scroll to 
                     learn more about the event!</p>
                 <button>Get Tickets</button>
+            </div>
+
+            <div className='landing-schedule-container'>
+                <h3>Schedule</h3>
+
+            </div>
+            <div className='landing-venue-container'>
+                <h3>Venue</h3>
+                <div className='landing-venue-img-container'>
+                    <img></img>
+                </div>
+                
+                <p>Our venue for this year's conference will be Kane Hall, which houses an auditorium style room that can seat up to 700 people. This room has the technological capability to record high quality video and audio of the stage. 
+                    Kane Hall is also in a centralized location on campus, providing easy access to our intended audience. Additionally, the venue provides adequate accommodations for those in need of wheelchair seating or closed captions.
+                    </p>
+                <p><a>Parking Information</a> | <a>Directions</a></p>
+                
+            </div>
+            <div className='landing-team-container'>
+                <h3>Meet the Team</h3>
+                <div className='landing-team-img-container'>
+                    <img></img>
+                </div>
+                <p>Learn more about the team behind the conference!</p>
+                <button>Meet the Team!</button>
             </div>
 
         </div>
