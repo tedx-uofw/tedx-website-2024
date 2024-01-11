6f1753f9cdb41757e5d9755d00bd0ae91da66403
mmel4nie <87841303+mmel4nie@users.noreply.github.com>
2024-01-10 23:00:40 -0800

    speaker headings

---
+++ src/components/speakers/speakerData.js
@@ -0,0 +1,18 @@
+// import susieSharpe from "../assets/speaker-photos/suseSharpe.png"
+
+
+// export default [
+//     {
+//         img: susieSharpe,
+//         name: "Susie Sharpe",
+//         description: <p>
+//             Aramis O. Hamer is a visual artist and muralist in Seattle, WA. 
+//             Her large-scale, acrylic paintings are inspired by the cosmos, 
+//             music, nature, divine femininity, and the complexities in Black 
+//             culture. Aramis has exhibited her colorful creations throughout 
+//             Washington state. She also painted the “V” in Seattle’s Black 
+//             Lives Matter mural. This self taught, Neddy Award winning-artist 
+//             lets her imagination be her guide.
+//         </p>
+//     }
+// ]
--- src/components/speakers/speakers.js
+++ src/components/speakers/speakers.js
@@ -1,9 +1,21 @@
 import React from "react";
+import "./speakers.css";
+import star from "./speakersImg/ChristmasStar.png";
+import speakerInfo from './speakerData.js';
 
 function Speakers(){
   return (
       <div id="speakers">
-        <p>hi</p>
+        <div className="heading">
+          <img src={star} class="center-star"/>
+          <h1 className="heading-text">SPEAKERS</h1>
+        </div>
+
+        <div className="heading">
+          <img src={star} class="center-star"/>
+          <h1 className="heading-text">PERFORMERS</h1>
+        </div>
+
       </div>
   )
 }
