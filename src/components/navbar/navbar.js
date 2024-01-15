09926a8cd1dae00b3490355bd485f0eddab9b25f
tashfia <tashfia@cs.washington.edu>
2024-01-14 23:10:01 -0800

    nav functionality

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,34 +1,38 @@
 import React from "react";
 import logo from "./nav-img/logo.png";
-import { Link } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 function Navbar(){
-  const getTix = () => {
-    console.log("add ticket page when up");
-    // window.location = "url"
-  };
+
+  let navigate = useNavigate();
+  const routeChange = (page) =>{
+    let path = "/" + page;
+    navigate(path);
+  }
 
   return (
       <div id="navbar">
-       <section class="left-nav">
+       <section id="left-nav">
         {/* add image and then on click have it go to home page */}
         <a href="/">
           <img class="nav-logo" src={logo} alt="tedx-logo"/>
         </a>
        </section>
-       <section class="right-nav">
+
+       <section id="right-nav">
         {/* links to all other pages */}
         <a href="/speakers">Speakers</a>
         <a href="/sponsors">Sponsors</a>
 
         {/* add onClick functions for these frop down options */}
-        <select id="nav-dropdown">
-          <option value="about"><a href="/about-tedx">About Us</a></option>
-          <option value="team"><a href="/team">Team</a></option>
-          <option vale="past-events">Past Events</option>
+        <select id="nav-dropdown" onChange={e => routeChange(e.target.value)}>
+          <option value="about-tedx">About Us</option>
+          <option value="team">Team</option>
+          <option value="past-events">Past Events</option>
         </select>
 
-        <button id="get-tix-button" type="button" onClick={getTix}>Get Tickets</button>
+        {/* add on click functionality when tix page up */}
+        <button id="tix-button" type="button">Get Tickets</button>
 
        </section>
       </div>
