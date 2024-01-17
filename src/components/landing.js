0b64940df3a5d13154eec64634c8a3759e7e0682
tedx-uofw <tedxuofw@uw.edu>
2024-01-16 22:15:27 -0800

    added more style

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './landing.css';
 
-import logo from '../assets/CROSSROADS.png';
-import graphic from '../assets/crossroads-graphic.png';
-import kane from '../assets/kane-hall.png';
-import teamPhoto from '../assets/team-photo.png';
+import logo from '../assets/landing-page-pics/CROSSROADS.png';
+import graphic from '../assets/landing-page-pics/crossroads-graphic.png';
+import kane from '../assets/landing-page-pics/kane-hall.png';
+import teamPhoto from '../assets/landing-page-pics/team-photo.png';
+
+import starIcon from '../assets/ChristmasStar.svg';
 
 
 function Schedule(props) {
@@ -19,6 +21,7 @@ function Schedule(props) {
                     <p className="items-title">{props.title}</p>
                 </div>
             </div>
+            <hr className='schedule-divider'></hr>
         </li>
     )
 
@@ -47,7 +50,7 @@ function Landing(){
                     <p id='scroll-to-learn'>Scroll to learn more about the event!</p>
                 </div>
                 <div className='ticket-btn'>
-                    <button>Get Tickets</button>
+                    <button>GET TICKETS</button>
                 </div>
                 <div className='crossroads-graphic'>
                     <img src={graphic} alt='Crossroads Graphic'/>
@@ -57,41 +60,53 @@ function Landing(){
             </div>
 
             <div className='landing-schedule-container section-ctr'>
-                <h3 className='title'>SCHEDULE</h3>
+                <div className='box'>
+                    <img src={starIcon} alt='Star Icon' />
+                    <h3 className='title'>SCHEDULE</h3>
+                </div>
+
                 <div className='schedule-items-ctr'>
-                    <ul>
+                    <ul id='schedule-list'>
                         {items.map((item) => <Schedule name={item.name} title={item.title} time={item.time} /> )}
                     </ul>
                 </div>
             </div>
 
             <div className='landing-venue-container section-ctr'>
-                <h3 className='title'>VENUE</h3>
+                <div className='box'>
+                    <img src={starIcon} alt='Star Icon' />
+                    <h3 className='title'>VENUE</h3>
+                </div>
                 <div className='landing-venue-contents-ctr'>
                     <div className='landing-venue-img-container'>
                         <img src={kane} alt='Kane Hall Photo'/>
                     </div>
                     <div className='landing-venue-text-ctr'>
-                        <p>Our venue for this year's conference will be Kane Hall, which houses an auditorium
+                        <p className='venue-text'>Our venue for this year's conference will be Kane Hall, which houses an auditorium
                             style room that can seat up to 700 people. This room has the technological capability
                             to record high quality video and audio of the stage.</p>
-                            <p>Kane Hall is also in a centralized location on campus,
+                            <p className='venue-text'>Kane Hall is also in a centralized location on campus,
                                 providing easy access to our intended audience. Additionally, the venue provides
                                 adequate accommodations for those in need of wheelchair seating or closed captions.</p>
-                        <p><a>Parking Information</a> | <a>Directions</a></p>
+                        <p className='parking-text'><a href='https://transportation.uw.edu/park/visitor'>Parking Information</a>
+                         | <a href='https://www.google.com/maps/place/Kane+Hall/@47.6566273,-122.3091503,15z/data=!4m6!3m5!1s0x549015bbf61d1867:0x39a352937b07450a!8m2!3d47.6566273!4d-122.3091503!16s%2Fg%2F11j111b43t?entry=ttu'>
+                            Directions</a></p>
                     </div>
                 </div>
             </div>
 
             <div className='landing-team-container section-ctr'>
-                <h3 className='title'>MEET THE TEAM</h3>
+            <div className='box'>
+                    <img src={starIcon} alt='Star Icon' />
+                    <h3 className='title'>MEET THE TEAM</h3>
+                </div>
                 <div className='landing-team-contents-ctr'>
                     <div className='landing-team-img-container'>
                         <img src={teamPhoto} alt='Team Photo'/>
                     </div>
                     <div className='landing-team-text-ctr'>
                         <p id='landing-team-text'>Learn more about the team behind the conference!</p>
-                        <button>Meet the Team!</button>
+                        <button className='btn'>MEET THE TEAM</button>
                     </div>
 
                 </div>
