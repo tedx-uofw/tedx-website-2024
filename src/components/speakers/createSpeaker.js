373786a75fbf27ae4241358fafab25dd7c6006f3
mmel4nie <87841303+mmel4nie@users.noreply.github.com>
2024-01-18 01:09:22 -0800

    switch speaker to px and fix opacity

--- src/components/speakers/createSpeaker.js
+++ src/components/speakers/createSpeaker.js
@@ -5,7 +5,7 @@ import "./createSpeaker.css";
 function CreateSpeaker(props) {
     return (
         <section>
-          <div className="speaker-content speaker-box">
+          <div className="speaker-box">
               <a href="https://www.w3schools.com"><img className="speaker-img" src={props.img} alt={props.name}/></a>
               <div className="speaker-profile">
                   <p className="speaker-name">{props.name}</p>
