b912853c0014086aae7312774c2ab679c47f2a37
tedx-uofw <tedxuofw@uw.edu>
2024-01-29 15:41:54 -0800

    fixed up team page a bit

--- src/components/about/team/about_team.jsx
+++ src/components/about/team/about_team.jsx
@@ -17,64 +17,69 @@ function AboutTeam() {
 
     return (
         <div class="about-team-container">
-            <h1 className="page-title">MEET THE TEAM</h1>
-
                 <div className="team-contents-container">
                     <div className="section-links">
                         <h2 id="teams-title">TEAMS</h2>
-                        <ul className="team-list-items">
-                            <li>
-                                <a href="#CO-PRESIDENTS" className="links">Co-Presidents</a>
-                            </li>
-                            <li>
-                                <a href="#DESIGN" className="links">Design</a>
-                            </li>
-                            <li>
-                                <a href="#WEB DEVELOPMENT" className="links">Web Dev</a>
-                            </li>
-                            <li>
-                                <a href="#LOGISTICS" className="links">Logistics</a>
-                            </li>
-                            <li>
-                                <a href="#MARKETING" className="links">Marketing</a>
-                            </li>
-                            <li>
-                                <a href="#FINANCE" className="links">Finance</a>
-                            </li>
-                            <li>
-                                <a href="#SPEAKER SELECTION" className="links">Speaker Selection</a>
-                            </li>
-                        </ul>
+                        <div className="section-links-content">
+                            <hr className="vertile-rule"></hr>
+                            <ul className="team-list-items">
+                                <li>
+                                    <a href="#CO-PRESIDENTS" className="links">Co-Presidents</a>
+                                </li>
+                                <li>
+                                    <a href="#DESIGN" className="links">Design</a>
+                                </li>
+                                <li>
+                                    <a href="#WEB DEVELOPMENT" className="links">Web Dev</a>
+                                </li>
+                                <li>
+                                    <a href="#LOGISTICS" className="links">Logistics</a>
+                                </li>
+                                <li>
+                                    <a href="#MARKETING" className="links">Marketing</a>
+                                </li>
+                                <li>
+                                    <a href="#FINANCE" className="links">Finance</a>
+                                </li>
+                                <li>
+                                    <a href="#SPEAKER SELECTION" className="links">Speaker Selection</a>
+                                </li>
+                            </ul>
                         </div>
-                    <div className="team-ctr">
-                        <Team
-                            name="CO-PRESIDENTS"
-                            data={presData}
-                            />
-                        <Team
-                            name="DESIGN"
-                            data={designData}
-                            />
-                        <Team
-                            name="WEB DEVELOPMENT"
-                            data={webdevData}
-                            />
-                        <Team
-                            name="LOGISTICS"
-                            data={logisticsData}
-                            />
-                        <Team
-                            name="MARKETING"
-                            data={marketingData}
-                            />
-                        <Team
-                            name="FINANCE"
-                            data={financeData}
-                            />
-                        <Team
-                            name="SPEAKER SELECTION"
-                            data={speakerData}
-                            />
+
+                    </div>
+                        <div className="mid-section-ctr">
+                            <h1 className="page-title">MEET THE TEAM</h1>
+                            <div className="team-ctr">
+                            <Team
+                                name="CO-PRESIDENTS"
+                                data={presData}
+                                />
+                            <Team
+                                name="DESIGN"
+                                data={designData}
+                                />
+                            <Team
+                                name="WEB DEVELOPMENT"
+                                data={webdevData}
+                                />
+                            <Team
+                                name="LOGISTICS"
+                                data={logisticsData}
+                                />
+                            <Team
+                                name="MARKETING"
+                                data={marketingData}
+                                />
+                            <Team
+                                name="FINANCE"
+                                data={financeData}
+                                />
+                            <Team
+                                name="SPEAKER SELECTION"
+                                data={speakerData}
+                                />
+                            </div>
                         </div>
                 </div>
         </div>
