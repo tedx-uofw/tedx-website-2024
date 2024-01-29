7ce342d093cd608e3e402561253dc2fc571c234a
tashfia <tashfia@cs.washington.edu>
2024-01-29 10:54:48 -0800

    drop-down

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import logo from "./nav-img/logo.png";
-import { useNavigate } from "react-router-dom";
+import arrow from "./nav-img/arrow.png";
+
+// import { useNavigate } from "react-router-dom";
 import "./navbar.css";
 
 function Navbar(){
 
-  let navigate = useNavigate();
-  const routeChange = (page) =>{
-    let path = "/" + page;
-    navigate(path);
-  }
+  // let navigate = useNavigate();
+  // const routeChange = (page) =>{
+  //   let path = "/" + page;
+  //   navigate(path);
+  // }
+  const [isHovered, setHover] = useState(true);
+
 
   return (
       <div id="navbar">
@@ -23,17 +27,32 @@ function Navbar(){
        <section id="right-nav">
         {/* links to all other pages */}
         <section id="right-nav-links">
-          <a class="right-nav" href="/speakers">SPEAKERS</a>
+          <a class="right-nav" id="first-link" href="/speakers">SPEAKERS</a>
           <a class="right-nav" href="/sponsors">SPONSORS</a>
 
           {/* add onClick functions for these frop down options */}
-          <select id="nav-dropdown" class="right-nav" onChange={e => routeChange(e.target.value)}>
+          {/* <select id="nav-dropdown" class="right-nav" onChange={e => routeChange(e.target.value)}>
               <option value="about-tedx" id="dropdown-default">ABOUT US</option>
               <option value="team">TEAM</option>
               <option value="past-events">PAST EVENTS</option>
-          </select>
+          </select> */}
+
+          <div class="dropdown">
+            <section id="dropdown-arrow">
+              <p class="dropbtn" onMouseEnter={() => {setHover(false)}} onMouseLeave={() => {setHover(true)}}>ABOUT</p>
+              <img className={isHovered ? "arrow"  : "rotated-arrow"} src= {arrow} alt="arrow"/>
+            </section>
+            <div className={isHovered ? "dropdown-content" : "hover-dropdown-content"}>
+              <a href="/about-tedx" onMouseEnter={() => {setHover(false)}} onMouseLeave={() => {setHover(true)}}>ABOUT US</a>
+              <a href="/team" onMouseEnter={() => {setHover(false)}} onMouseLeave={() => {setHover(true)}}>TEAM</a>
+              <a href="/past-events" onMouseEnter={() => {setHover(false)}} onMouseLeave={() => {setHover(true)}}>PAST EVENTS</a>
+            </div>
+          </div>
+
 
         </section>
+
+
 
 
         {/* add on click functionality when tix page up */}
