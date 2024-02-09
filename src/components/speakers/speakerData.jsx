b87744729948cf7496c4981f4987898ad13927e9
mmel4nie <87841303+mmel4nie@users.noreply.github.com>
2024-02-09 00:44:02 -0800

    add speaker descr

--- src/components/speakers/speakerData.jsx
+++ src/components/speakers/speakerData.jsx
@@ -4,30 +4,73 @@ import ExImg from "./speakersImg/Rectangle 24.png"
 const speakerArray = [
     {
         img: ExImg,
-        name: "Susie Sharpe",
-        title: "Visual artist & Muralist",
+        name: "Dr. Noha Polack",
+        title: "Board-Certified Pediatrician",
         description: <p>
-            Aramis O. Hamer is a visual artist and muralist in Seattle, WA. 
-            Her large-scale, acrylic paintings are inspired by the cosmos, 
-            music, nature, divine femininity, and the complexities in Black 
-            culture. Aramis has exhibited her colorful creations throughout 
-            Washington state. She also painted the “V” in Seattle’s Black 
-            Lives Matter mural. This self taught, Neddy Award winning-artist 
-            lets her imagination be her guide.
+            Dr. Noha Polack is a board-certified pediatrician who specializes 
+            in helping teens and pre-teens with body image and weight issues. 
+            She empowers them to help them love their bodies and take control 
+            of their health journey. She teaches professionals/parents to communicate 
+            positive messages about health instead of focusing on weight.
         </p>
     },
     {
         img: ExImg,
-        name: "Susie Sharpe",
-        title: "Visual artist & Muralist",
+        name: "Emily Lago Anderson",
+        title: "Computer Engineer and Educator",
         description: <p>
-            Aramis O. Hamer is a visual artist and muralist in Seattle, WA. 
-            Her large-scale, acrylic paintings are inspired by the cosmos, 
-            music, nature, divine femininity, and the complexities in Black 
-            culture. Aramis has exhibited her colorful creations throughout 
-            Washington state. She also painted the “V” in Seattle’s Black 
-            Lives Matter mural. This self taught, Neddy Award winning-artist 
-            lets her imagination be her guide.
+            Emily Lago Anderson is a computer engineer and educator with a passion 
+            for eradicating the racial equity gap in tech. She is pursuing her M.Ed. 
+            in Learning Sciences and Human Development at UW to develop equitable 
+            tech education. In her work, Emily is a Support Escalation Engineer at 
+            Microsoft and Founder of Evo Learning Academy.
+        </p>
+    },
+    {
+        img: ExImg,
+        name: "Chirag Shah",
+        title: "University of Washington Professor",
+        description: <p>
+            Chirag Shah is a Professor at the University of Washington. He is the 
+            Founding Director of InfoSeeking Lab and Founding Co-Director of the 
+            Center for Responsibility in AI Systems & Experiences (RAISE). His 
+            research focuses on building, auditing, and correcting intelligent 
+            information access systems. Shah is a Distinguished Member of ACM.
+        </p>
+    },
+    {
+        img: ExImg,
+        name: "Dr. Zarya Rubin",
+        title: "Medicine Physician",
+        description: <p>
+            Dr. Zarya Rubin is a Harvard-educated functional medicine physician, 
+            keynote speaker, and burnout expert. She helps high-achieving women 
+            get to the root cause of chronic symptoms and stress so they can heal 
+            from the inside out and truly thrive. She is the founder and CEO of Dr. 
+            Zarya, a boutique functional medicine practice in Portland, OR.
+        </p>
+    },
+    {
+        img: ExImg,
+        name: "Jeff Nguyen",
+        title: "BobaTalks",
+        description: <p>
+            Jeff Nguyen is most known for his work with BobaTalks, a nonprofit that 
+            helps students navigate the early career ambiguities of their lives. He 
+            spends most of my time taking photos, planning weddings, and helping 
+            students figure out their lives, one boba chat at a time.
+        </p>
+    },
+    {
+        img: ExImg,
+        name: "Nash Fung",
+        title: "Keynote Speaker & Magician",
+        description: <p>
+            Nash Fung is a keynote speaker & magician who empowers his audiences 
+            through his engaging keynotes. Drawing from his 16 + years of experience 
+            as a crisis line volunteer, he now unveils the curtain on the magic of 
+            empathetic communication, teaching leaders how to lead with empathy and 
+            make others feel heard, understood, valued, and empowered.
         </p>
     }
 ]
