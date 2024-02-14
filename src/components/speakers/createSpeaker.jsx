f6504dd4ee0ef20a3c6a5cc01b6b755f03275c30
mmel4nie <87841303+mmel4nie@users.noreply.github.com>
2024-02-14 00:51:27 -0800

    take out nav links

--- src/components/speakers/createSpeaker.jsx
+++ src/components/speakers/createSpeaker.jsx
@@ -5,7 +5,7 @@ function CreateSpeaker(props) {
     return (
         <section>
           <div className="speaker-box">
-            <a href="https://www.w3schools.com"><img className="speaker-photo" src={props.img} alt={props.name}/></a>
+            <img className="speaker-photo" src={props.img} alt={props.name}/>
             <div className="speaker-content">
               <p className="speaker-name">{props.name}</p>
               <p className="speaker-title">{props.title}</p>
