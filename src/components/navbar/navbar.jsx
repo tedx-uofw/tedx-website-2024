c97e808d7cd41b65036b70e2ab89a0f74dab227e
tashfia <tashfia@cs.washington.edu>
2024-03-07 00:17:46 -0800

    button

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -25,6 +25,7 @@ function Navbar(){
   // }
 
   // const [width, setWidth] = useState(window.innerWidth);
+  // adapt for touch
   let prevScroll = window.scrollY;
   window.onscroll = function() {
     let scroll = window.scrollY;
@@ -82,7 +83,9 @@ function Navbar(){
 
         {/* add on click functionality when tix page up */}
         <div className='ticket-btn'>
-                    <button id="nav-button" onClick={routeToTicketLink}>GET TICKETS</button>
+                    <button id="nav-button" className='btn'>
+                    <a href='https://docs.google.com/forms/d/e/1FAIpQLScFKzPkloRcReyNhadtJzJZ9HuH_g9o7tsom5HJczat4zWp0A/viewform' className='tickets-link'>GET TICKETS</a>
+                    </button>
                 </div>
 
        </section>
@@ -90,10 +93,16 @@ function Navbar(){
        <svg id={isClicked ? "side-menu-icon" : "side-menu-icon-clicked"} onClick={() => {setClick(false)}} xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24"><path fill="currentColor" d="M4 18h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1m0-5h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1M3 7c0 .55.45 1 1 1h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1"/></svg>
           {/* <img id={isClicked ? "side-menu-icon" : "side-menu-icon-clicked"} src={menu} onClick={() => {setClick(false)}} alt="side-menu-icon"/> */}
           <div className={isClicked ? "side-menu-content" : "side-menu-content-clicked"}>
-            <div className='ticket-btn'>
-                <button id="nav-button">GET TICKETS</button>
-                <img id="exit-button" src={exit} onClick={() => {setClick(true)}} alt="exit-button"/>
+          <div className="ticket-btn-row">
+          <div className='ticket-btn'>
+              <button id="nav-button" className='btn'>
+                    <a href='https://docs.google.com/forms/d/e/1FAIpQLScFKzPkloRcReyNhadtJzJZ9HuH_g9o7tsom5HJczat4zWp0A/viewform' className='tickets-link'>GET TICKETS</a>
+                </button>
             </div>
+            <img id="exit-button" src={exit} onClick={() => {setClick(true)}} alt="exit-button"/>
+
+          </div>
+
             <a id="first-link" href="/">HOME</a>
             <a href="/speakers">SPEAKERS</a>
             <a href="/sponsors">SPONSORS</a>
