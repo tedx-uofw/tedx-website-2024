d0a870c2e26d3abe764bc9df845d81f745ffe090
Harmony Shirk <harmonyshirk@gmail.com>
2024-03-07 00:11:10 -0800

    testing sponsors page

---
+++ src/components/about/team/team-data/ro-data.jsx
@@ -0,0 +1,11 @@
+import ro from '../../../../assets/team-member-pics/ro 1.png';
+
+const data =
+[
+    {
+        name: "RO VERDEJA",
+        title: "Storytelling Presentation Coach",
+        img: ro
+    }
+]
+export default data;
