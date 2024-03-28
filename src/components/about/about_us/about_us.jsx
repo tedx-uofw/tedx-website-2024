02e2950eedbf94a0510646388724921ac934914e
tashfia <tashfia@cs.washington.edu>
2024-03-28 16:55:43 -0700

    clean

--- src/components/about/about_us/about_us.jsx
+++ src/components/about/about_us/about_us.jsx
@@ -29,19 +29,19 @@ function AboutUs(){
           </div>
           <div class="content">
             <img src={ted} alt="TED Logo: Ideas worth spreading"></img>
-            <p class="mobile">TED is a nonprofit organization devoted to Ideas Worth Spreading. Started as a four-day conference in 
-              California 30 years ago, TED has grown to support its mission with multiple initiatives. The two annual 
+            <p class="mobile">TED is a nonprofit organization devoted to Ideas Worth Spreading. Started as a four-day conference in
+              California 30 years ago, TED has grown to support its mission with multiple initiatives. The two annual
               TED conferences invite the world's leading thinkers and doers to Vancouver, British Columbia to speak for 18 minutes or less.</p>
-            
-            <p class="scroll">TED is on a mission to discover and spread ideas that spark imagination, embrace possibility and catalyze impact. Our organization is devoted to curiosity, reason, wonder and the pursuit of knowledge — without an agenda. 
+
+            <p class="scroll">TED is on a mission to discover and spread ideas that spark imagination, embrace possibility and catalyze impact. Our organization is devoted to curiosity, reason, wonder and the pursuit of knowledge — without an agenda.
               We welcome people from every discipline and culture who seek a deeper understanding of the world and connection with others, and we invite everyone to engage with ideas and activate them in your community.
-              TED began in 1984 as a conference where Technology, Entertainment and Design converged, but today it spans a multitude of worldwide communities and initiatives exploring everything from science and business to education, 
-              arts and global issues. In addition to the hundreds of TED Talks curated from our annual conferences and published on TED.com, we produce original podcasts, short video series, animated educational lessons 
-              (TED-Ed) and TV programs that are translated into more than 100 languages and distributed via partnerships around the world. Each year, more than 3,000 independently run TEDx events bring people together to share 
-              ideas and bridge divides in communities on every continent. Through the Audacious Project, TED has helped catalyze more than $3 billion in funding for projects that seek to make the world more beautiful, sustainable and just. 
+              TED began in 1984 as a conference where Technology, Entertainment and Design converged, but today it spans a multitude of worldwide communities and initiatives exploring everything from science and business to education,
+              arts and global issues. In addition to the hundreds of TED Talks curated from our annual conferences and published on TED.com, we produce original podcasts, short video series, animated educational lessons
+              (TED-Ed) and TV programs that are translated into more than 100 languages and distributed via partnerships around the world. Each year, more than 3,000 independently run TEDx events bring people together to share
+              ideas and bridge divides in communities on every continent. Through the Audacious Project, TED has helped catalyze more than $3 billion in funding for projects that seek to make the world more beautiful, sustainable and just.
               In 2020, TED launched Countdown, an initiative to accelerate solutions to the climate crisis and mobilize a movement for a net-zero future. View a full list of TED’s many programs and initiatives.
               <br></br><br></br>TED is owned by a nonprofit, nonpartisan foundation. Our aim is to help create a future worth pursuing for all.
-              <br></br><br></br>Follow TED on <a href="https://twitter.com/TEDTalks">Twitter</a>, <a href="https://www.facebook.com/TED">Facebook</a>, 
+              <br></br><br></br>Follow TED on <a href="https://twitter.com/TEDTalks">Twitter</a>, <a href="https://www.facebook.com/TED">Facebook</a>,
               <a href="https://www.instagram.com/ted/"> Instagram</a>, <a href="https://www.tiktok.com/@tedtoks">TikTok</a> and on <a href="https://www.linkedin.com/company/ted-conferences/">LinkedIn</a>.</p>
           </div>
         </div>
@@ -55,7 +55,7 @@ function AboutUs(){
           <div class="content">
             <img src={newPhoto} alt="TEDx Logo"></img>
             <p>In the spirit of ideas worth spreading, TEDx is a program of local, self-organized events that bring people together to share a TED-like experience.<br></br><br></br>
-              At a TEDx event, TED Talks video and live speakers combine to spark deep discussion and connection. These local, self-organized events are branded TEDx, 
+              At a TEDx event, TED Talks video and live speakers combine to spark deep discussion and connection. These local, self-organized events are branded TEDx,
               where x = independently organized TED event.<br></br><br></br>The TED Conference provides general guidance for the TEDx program, but individual TEDx events are self-organized.</p>
           </div>
         </div>
@@ -68,11 +68,11 @@ function AboutUs(){
           </div>
           <div class="content">
             <img src={tedxuofw} alt="TEDxUofW Logo"></img>
-            <p>TEDxUofW was established to bring inspirational and informative TED style talks to the University of Washington. Since 2012, our student-run organization 
+            <p>TEDxUofW was established to bring inspirational and informative TED style talks to the University of Washington. Since 2012, our student-run organization
               has sought to give amazing speakers a receptive audience to share their passions with. </p>
           </div>
         </div>
-        
+
         {/* Contact Us */}
         <div id="contact">
           <div class="aboutus-header">
@@ -85,7 +85,7 @@ function AboutUs(){
                 {/* Instagram */}
                 <a href="https://www.instagram.com/tedxuofw/"><img src={instagram}></img>@tedxuofw</a>
                 {/* TikTok */}
-                <a href="https://www.tiktok.com/@tedxuofw"><img src={tiktok}></img>@tedxuofw</a>                
+                <a href="https://www.tiktok.com/@tedxuofw"><img src={tiktok}></img>@tedxuofw</a>
                 {/* Facebook */}
                 <a href="https://www.facebook.com/TEDxUofW"><img src={facebook}></img>@tedxuofw</a>
                 {/* LinkedIn */}
