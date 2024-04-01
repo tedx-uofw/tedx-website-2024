0d14600a59238d4255ace91453507f189b7b62e9
tedx-uofw <tedxuofw@uw.edu>
2024-04-01 13:28:09 -0700

    fix speakers titles

--- src/components/speakers/speakerData.jsx
+++ src/components/speakers/speakerData.jsx
@@ -13,10 +13,10 @@ const speakerArray = [
         title: "Engineer and Educational Equity Activist",
         talk: "The Purpose in Daydreaming",
         description: <p>
-            Emily Lago Anderson is a computer engineer and educator with a passion 
-            for eradicating the racial equity gap in tech. She is pursuing her M.Ed. 
-            in Learning Sciences and Human Development at UW to develop equitable 
-            tech education. In her work, Emily is a Support Escalation Engineer at 
+            Emily Lago Anderson is a computer engineer and educator with a passion
+            for eradicating the racial equity gap in tech. She is pursuing her M.Ed.
+            in Learning Sciences and Human Development at UW to develop equitable
+            tech education. In her work, Emily is a Support Escalation Engineer at
             Microsoft and Founder of Evo Learning Academy.
         </p>
     },
@@ -24,12 +24,12 @@ const speakerArray = [
         img: chiragShah,
         name: "Chirag Shah, PhD",
         title: "Professor in Information School",
-        talk: "Humanity at the Crossroads of What AI Could, Should, and Would Do",
+        talk: "What AI Could, Should, and Would Do",
         description: <p>
-            Chirag Shah is a Professor at the University of Washington. He is the 
-            Founding Director of InfoSeeking Lab and Founding Co-Director of the 
-            Center for Responsibility in AI Systems & Experiences (RAISE). His 
-            research focuses on building, auditing, and correcting intelligent 
+            Chirag Shah is a Professor at the University of Washington. He is the
+            Founding Director of InfoSeeking Lab and Founding Co-Director of the
+            Center for Responsibility in AI Systems & Experiences (RAISE). His
+            research focuses on building, auditing, and correcting intelligent
             information access systems. Shah is a Distinguished Member of ACM.
         </p>
     },
@@ -39,10 +39,10 @@ const speakerArray = [
         title: "Pediatrician",
         talk: "Recognizing the Weight Bias Within",
         description: <p>
-            Dr. Noha Polack is a board-certified pediatrician who specializes 
-            in helping teens and pre-teens with body image and weight issues. 
-            She empowers them to help them love their bodies and take control 
-            of their health journey. She teaches professionals/parents to communicate 
+            Dr. Noha Polack is a board-certified pediatrician who specializes
+            in helping teens and pre-teens with body image and weight issues.
+            She empowers them to help them love their bodies and take control
+            of their health journey. She teaches professionals/parents to communicate
             positive messages about health instead of focusing on weight.
         </p>
     },
@@ -52,10 +52,10 @@ const speakerArray = [
         title: "Keystone Speaker & Magician",
         talk: "Unlocking the Magic to Providing Better Emotional Support",
         description: <p>
-            Nash Fung is a keynote speaker & magician who empowers his audiences 
-            through his engaging keynotes. Drawing from his 16+ years of experience 
-            as a crisis line volunteer, he now unveils the curtain on the magic of 
-            empathetic communication, teaching leaders how to lead with empathy and 
+            Nash Fung is a keynote speaker & magician who empowers his audiences
+            through his engaging keynotes. Drawing from his 16+ years of experience
+            as a crisis line volunteer, he now unveils the curtain on the magic of
+            empathetic communication, teaching leaders how to lead with empathy and
             make others feel heard, understood, valued, and empowered.
         </p>
     },
@@ -65,10 +65,10 @@ const speakerArray = [
         title: "Functional Medicine Physician",
         talk: "Four Words That Can Protect You From Burnout",
         description: <p>
-            Dr. Zarya Rubin is a Harvard-educated functional medicine physician, 
-            keynote speaker, and burnout expert. She helps high-achieving women 
-            get to the root cause of chronic symptoms and stress so they can heal 
-            from the inside out and truly thrive. She is the founder and CEO of Dr. 
+            Dr. Zarya Rubin is a Harvard-educated functional medicine physician,
+            keynote speaker, and burnout expert. She helps high-achieving women
+            get to the root cause of chronic symptoms and stress so they can heal
+            from the inside out and truly thrive. She is the founder and CEO of Dr.
             Zarya, a boutique functional medicine practice in Portland, OR.
         </p>
     },
@@ -76,11 +76,11 @@ const speakerArray = [
         img: jeffNguyen,
         name: "Jeff Nguyen",
         title: "Founder @ BobaTalks",
-        talk: "Clarity Amidst Crossroads: Navigating Your North Star",
+        talk: "Finding Clarity: Navigating Your North Star",
         description: <p>
-            Jeff Nguyen is most known for his work with BobaTalks, a nonprofit that 
-            helps students navigate the early career ambiguities of their lives. He 
-            spends most of my time taking photos, planning weddings, and helping 
+            Jeff Nguyen is most known for his work with BobaTalks, a nonprofit that
+            helps students navigate the early career ambiguities of their lives. He
+            spends most of my time taking photos, planning weddings, and helping
             students figure out their lives, one boba chat at a time.
         </p>
     }
